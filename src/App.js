1e933bf8ffd02d035cf288c3b5b934f8e4c9c87b
Taylor Sam <taylorsam209@yahoo.com>
2018-04-05 17:00:45 -0700

    LogOut button functional, app complete

--- src/App.js
+++ src/App.js
@@ -1,39 +1,51 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
-import firebase from 'firebase';
+import firebase from "firebase";
 import { Header, Button, Spinner } from "./components/common"; //Imported from index.js within common folder when no file is specified.
 import LoginForm from "./components/LoginForm";
 
 class App extends Component {
   state = {
     loggedIn: null
+  };
+
+  componentDidMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyB0Q40cb4B3nP1Ulq8bmjUgmp6H8JkjU5M",
+      authDomain: "auth-f4849.firebaseapp.com",
+      databaseURL: "https://auth-f4849.firebaseio.com",
+      projectId: "auth-f4849",
+      storageBucket: "auth-f4849.appspot.com",
+      messagingSenderId: "1074497350321"
+    });
+
+    firebase.auth().onAuthStateChanged(user => {
+      return user
+        ? this.setState({ loggedIn: true })
+        : this.setState({ loggedIn: false });
+    });
   }
 
-    componentDidMount() {
-        firebase.initializeApp( {
-            apiKey: "AIzaSyB0Q40cb4B3nP1Ulq8bmjUgmp6H8JkjU5M",
-            authDomain: "auth-f4849.firebaseapp.com",
-            databaseURL: "https://auth-f4849.firebaseio.com",
-            projectId: "auth-f4849",
-            storageBucket: "auth-f4849.appspot.com",
-            messagingSenderId: "1074497350321"
-          });
-
-        firebase.auth().onAuthStateChanged((user)=> {
-          return user ? this.setState({loggedIn: true}): this.setState({loggedIn: false})
-        });
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <View style={styles.buttonContainer}>
+            <Button onPress={()=>firebase.auth().signOut()}>Log Out</Button>
+          </View>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return (
+          <View style={styles.spinner}>
+            <Spinner size="large" />
+          </View>
+        );
     }
+  }
 
-    renderContent() {
-      switch(this.state.loggedIn) {
-        case true:
-          return <View style={styles.buttonContainer}><Button>Log Out</Button></View>
-        case false:
-          return <LoginForm />
-        default:
-        return <View style={styles.spinner}><Spinner size='large' /></View>
-      }
-    }
+
 
   render() {
     return (
@@ -51,8 +63,8 @@ const styles = {
     flexDirection: "row"
   },
   spinner: {
-    marginTop:100
+    marginTop: 100
   }
-}
+};
 
 export default App;
